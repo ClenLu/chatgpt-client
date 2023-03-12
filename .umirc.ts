eecbbe286fa183a72c8540cd35de37bb54254097
sorrycc <sorrycc@gmail.com>
2023-03-12 21:42:54 +0800

    Initial commit

---
+++ .umirc.ts
@@ -0,0 +1,11 @@
+export default {
+  tailwindcss: {},
+  valtio: {},
+  reactQuery: {},
+  plugins: [
+    "@umijs/plugins/dist/tailwindcss",
+    "@umijs/plugins/dist/valtio",
+    "@umijs/plugins/dist/react-query",
+  ],
+  icons: {},
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { proxy, useQuery } from 'umi';
+
+proxy;
+useQuery;
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-red-500">Page index</h1>
+    </div>
+  );
+}
